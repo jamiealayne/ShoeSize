7a771adcb6142ec5dada3f0fb08080be02f59ebb
Jamie Layne <jamielayne@Jamies-MacBook-Pro.local>
2014-09-16 09:12:02 -0400

    ShoeSize App first commit

--- ShoeSize/ViewController.swift
+++ ShoeSize/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	
+	@IBOutlet var shoeSizeField: UITextField!
+	@IBOutlet var shoeSizeLabel: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func convertSize(sender: AnyObject) {
+		let shoeSize = shoeSizeField.text.toInt()
+		let conversionConstant = 30
+		var euroSize = shoeSize! + conversionConstant
+		
+		shoeSizeLabel.hidden = false
+		shoeSizeLabel.text = "Men's shoe size in Europe: \(euroSize)"
+		
+	}
 
 }
 
